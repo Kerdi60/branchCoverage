51aca00b95863af78418d24ec6eed0c87620073a
Kerdi60 <keremkazandir@gmail.com>
2024-03-31 08:43:52 +0300

    test files

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -12,6 +12,30 @@ public class TestCompute {
   public void example() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
-    assertTrue(true);
+    when(mq.size()).thenReturn(0);
+    assertEquals(-1, c.countNumberOfOccurrences(""));
+  }
+
+  @Test
+  public void isContain() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(10);
+    when(mq.contains("deneme")).thenReturn(false);
+    c = new Compute(mq);
+    assertEquals(0, c.countNumberOfOccurrences("deneme"));
+
+  }
+
+  @Test
+  public void normal() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(10);
+    when(mq.contains("deneme")).thenReturn(true);
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0)
+        when(mq.getAt(i)).thenReturn("deneme");
+    }
+    c = new Compute(mq);
+    assertEquals(5, c.countNumberOfOccurrences("deneme"));
   }
 }
--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -9,8 +9,30 @@ public class TestUtil {
   Util c;
 
   @Before
-  public void setUp() { c = new Util(); }
+  public void setUp() {
+    c = new Util();
+  }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void oneLength() {
+    assertFalse(c.compute(1));
+  }
+
+  @Test
+  public void evenLength() {
+    assertFalse(c.compute(1, 2, 3, 4));
+    assertFalse(c.compute());
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void runTime() throws RuntimeException {
+    c.compute(1, 0, 1);
+  }
+
+  @Test
+  public void Divides() {
+    assertTrue(c.compute(1, 2, 3, 4, 5));
+    assertFalse(c.compute(7, 9, 10));
+  }
+
 }
